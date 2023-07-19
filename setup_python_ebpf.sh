f337fff94f5b32c97a7d2c89732ec864c74ef748
Pansy Ng <37123569+pansyng@users.noreply.github.com>
2023-07-19 11:33:45 -0700

    Update setup_python_ebpf.sh

--- setup_python_ebpf.sh
+++ setup_python_ebpf.sh
@@ -46,5 +46,4 @@ sudo make install
 popd
 # Download the source code
 wget https://gist.githubusercontent.com/oghie/b4e3accf1f87afcb939f884723e2b462/raw/fe60e6b66135640ea39c878589fb092b6eb838a1/final_code_eBPF_dns.py
-# run 
-sudo python3 final_code_eBPF_dns.py
+
