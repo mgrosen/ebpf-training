b8406e8591359c9c1c7d9f5425f8a8c3f459905c
mgrosen <mgrosen@github.com>
2023-07-19 22:13:12 -0700

    some minor updates

--- post_porcess.py
+++ post_porcess.py
@@ -1,6 +1,7 @@
 input_str = """GET pypi.org /simple/foo
 GET pypi.org /simple/bar
-GET pypi.org /simple/baz"""
+GET pypi.org /simple/baz
+POST example.com /api/foo"""
 
 host_dict = dict[str, set]()
 params = set()
@@ -10,6 +11,12 @@ for line in input_str.split("\n"):
         package_name = parts[2].split("/")[2]
         host_dict.setdefault(parts[1], set()).add(package_name)
 
-for host in host_dict:
-    print("from " + host + " there are " + str(len(host_dict[host])) + " packages: ")
-    print(host_dict[host])
+## for pypi
+print("from pypi.org there are %s packages downloaded in this workflow: " % str(len(host_dict['pypi.org'])))
+for package in host_dict['pypi.org']:
+    print("- %s" % package)
+
+print("")
+# for all hosts but pypi
+for host in [host for host in host_dict if host != 'pypi.org']:
+    print("%s url hitting %s in this workflow" % (str(len(host_dict[host])), host))
