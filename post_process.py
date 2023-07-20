43e8a80c1dc739031185332e34302e7faf28c415
mgrosen <mgrosen@github.com>
2023-07-20 14:10:22 -0700

    update post processing

---
+++ post_process.py
@@ -0,0 +1,30 @@
+import sys
+import re
+
+python_hosted_regex = r"/packages/.*/(.*?)-\d+\..*"
+
+host_dict = dict[str, set]()
+params = set()
+
+file_path = sys.argv[1]
+
+with open(file_path, 'r') as file:
+    for line in file:
+        parts = line.split()
+        if len(parts) >= 3:
+            url = parts[2]
+            host_dict.setdefault(parts[1], set()).add(url)
+
+for host in [host for host in host_dict if host != 'pypi.org']:
+    print("%s url hitting %s in this workflow" % (str(len(host_dict[host])), host))
+
+print("")
+
+## python packages
+print("from pypi.org there are %s packages downloaded in this workflow: " % str(len(host_dict['pypi.org']) + len(host_dict['files.pythonhosted.org'])))
+for url in host_dict['pypi.org']:
+    print("- %s" % url.split("/")[2])
+for url in host_dict['files.pythonhosted.org']:
+    match = re.search(python_hosted_regex, url)
+    if match:
+        print("- %s" % match.group(1))
