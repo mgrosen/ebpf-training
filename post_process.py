d4c58b607248ea1a3e9015f505b48a26493f8970
mgrosen <mgrosen@github.com>
2023-07-20 15:21:06 -0700

    update python code

--- post_process.py
+++ post_process.py
@@ -1,7 +1,5 @@
 import sys
 from py_markdown_table.markdown_table import markdown_table
-#import re
-#python_hosted_regex = r"/packages/.*/(.*?)-\d+\..*"
 
 host_dict = dict()
 params = set()
@@ -9,7 +7,8 @@ params = set()
 file_path = sys.argv[1]
 
 with open(file_path, 'r') as file:
-    for line in file:
+    lines = file.readlines()[3:]
+    for line in lines:
         parts = line.split()
         if len(parts) >= 3:
             url = parts[2]
@@ -25,11 +24,13 @@ markdown_ready_data = [{"Host": str(entry[1]), "Count": int(entry[0])} for entry
 markdown = markdown_table(markdown_ready_data).set_params(padding_weight = 'right').get_markdown()
 
 print('# Proccessed eBPF sniffed HTTPS traffic')
+print('')
 print("## Host query counts ")
+print('')
 print(markdown)
-print("")
+print('')
 ## python packages
-print(" ## Python packages downloaded in this workflow: ")
+print("## %s Python packages downloaded in this workflow" % len(host_dict['pypi.org']))
 sorted_python_packages = sorted(host_dict['pypi.org'], key=lambda x: x.split("/")[2])
 for url in sorted_python_packages:
     print("- %s" % url.split("/")[2])
