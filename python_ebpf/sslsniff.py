cf0d7fe817f932c5d31cf6ab04f8ec7e5e552861
mgrosen <mgrosen@github.com>
2023-07-20 13:21:51 -0700

    re:debug

--- python_ebpf/sslsniff.py
+++ python_ebpf/sslsniff.py
@@ -292,7 +292,7 @@ print(search_str)
 str_buff = create_string_buffer(b"Host: ")
 print(str_buff)
 search_str[0] = str_buff
-print(str([char for char in search_str]))
+print(str([str(cint) for cint in search_str]))
 
 # It looks like SSL_read's arguments aren't available in a return probe so you
 # need to stash the buffer address in a map on the function entry and read it
