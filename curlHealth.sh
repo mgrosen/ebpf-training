42af846e7bf7beda9403f50cb52de0987940c472
Raj R <rajrku@github.com>
2023-07-20 10:58:42 -0700

    curl

--- curlHealth.sh
+++ curlHealth.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+num=4
+
+if [ $# -eq 1 ]
+then
+    num = $1
+fi
+
+echo $num
+
 # Define a function to run in a separate thread
 function curl_function() {
     curl -i https://pipelinesghubeus20.actions.githubusercontent.com/_apis/health --http1.1
 }
 
 # Spawn multiple threads
-for i in {1..4}; do
+for i in {1..$num}; do
     curl_function $i &
 done
 
