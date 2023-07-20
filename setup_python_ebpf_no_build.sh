48c5b504a709bdad5ec079e1fc45b2603d68f94e
Tommaso Pieroncini <tommipier96@gmail.com>
2023-07-20 13:43:16 -0400

    Update setup_python_ebpf_no_build.sh

--- setup_python_ebpf_no_build.sh
+++ setup_python_ebpf_no_build.sh
@@ -32,5 +32,46 @@ sudo apt-get install -y bcc-tools libbcc-examples linux-headers-$(uname -r)
 
 sudo /usr/local/go/bin/go get -u ./...
 
-# mkdir python_ebpf 
-# wget -P python_ebpf/ https://gist.githubusercontent.com/oghie/b4e3accf1f87afcb939f884723e2b462/raw/fe60e6b66135640ea39c878589fb092b6eb838a1/final_code_eBPF_dns.py
+# Download bcc dependencies
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bcc-0.28.0+18b00a90-py3.8.egg -O /usr/lib/python3/dist-packages/bcc-0.28.0+18b00a90-py3.8.egg
+sudo touch /usr/lib/python3/dist-packages/easy-install.pth
+sudo echo "./bcc-0.28.0+18b00a90-py3.8.egg" | sudo tee -a /usr/lib/python3/dist-packages/easy-install.pth
+
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc.so.0.28.0 -O /usr/lib/x86_64-linux-gnu/libbcc.so.0.28.0
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc.so.0 -O /usr/lib/x86_64-linux-gnu/libbcc.so.0
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc.so -O /usr/lib/x86_64-linux-gnu/libbcc.so
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc.a -O /usr/lib/x86_64-linux-gnu/libbcc.a
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc-loader-static.a -O /usr/lib/x86_64-linux-gnu/libbcc-loader-static.a
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc_bpf.a -O /usr/lib/x86_64-linux-gnu/libbcc_bpf.a
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/file_desc.h -O /usr/include/bcc/file_desc.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/table_desc.h -O /usr/include/bcc/table_desc.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/table_storage.h -O /usr/include/bcc/table_storage.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bcc_common.h -O /usr/include/bcc/bcc_common.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bpf_module.h -O /usr/include/bcc/bpf_module.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bcc_exception.h -O /usr/include/bcc/bcc_exception.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bcc_syms.h -O /usr/include/bcc/bcc_syms.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bcc_proc.h -O /usr/include/bcc/bcc_proc.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bcc_elf.h -O /usr/include/bcc/bcc_elf.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bcc_usdt.h -O /usr/include/bcc/bcc_usdt.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/netdev.h -O /usr/include/bcc/compat/linux/netdev.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/pkt_sched.h -O /usr/include/bcc/compat/linux/pkt_sched.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/perf_event.h -O /usr/include/bcc/compat/linux/perf_event.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/netlink.h -O /usr/include/bcc/compat/linux/netlink.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/fcntl.h -O /usr/include/bcc/compat/linux/fcntl.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/openat2.h -O /usr/include/bcc/compat/linux/openat2.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/btf.h -O /usr/include/bcc/compat/linux/btf.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/if_link.h -O /usr/include/bcc/compat/linux/if_link.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/pkt_cls.h -O /usr/include/bcc/compat/linux/pkt_cls.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bpf_common.h -O /usr/include/bcc/compat/linux/bpf_common.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/if_xdp.h -O /usr/include/bcc/compat/linux/if_xdp.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bpf.h -O /usr/include/bcc/compat/linux/bpf.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc.pc -O /usr/lib/x86_64-linux-gnu/pkgconfig/libbcc.pc
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbpf.h -O /usr/include/bcc/libbpf.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/perf_reader.h -O /usr/include/bcc/perf_reader.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/bcc_version.h -O /usr/include/bcc/bcc_version.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc_bpf.so.0.28.0 -O /usr/lib/x86_64-linux-gnu/libbcc_bpf.so.0.28.0
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc_bpf.so.0 -O /usr/lib/x86_64-linux-gnu/libbcc_bpf.so.0
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/libbcc_bpf.so -O /usr/lib/x86_64-linux-gnu/libbcc_bpf.so
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/BPF.h -O /usr/include/bcc/BPF.h
+sudo wget https://ebpfsa.blob.core.windows.net/test-storage/BPFTable.h -O /usr/include/bcc/BPFTable.h
+
